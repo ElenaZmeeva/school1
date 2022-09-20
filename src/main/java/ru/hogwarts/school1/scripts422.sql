cf179ad60f57ff55f49ac9b8159a8c455b59a2c8
elenazmeeva <biertli@bk.ru>
2022-09-20 22:27:06 +0300

    HW4.2

---
+++ src/main/java/ru/hogwarts/school1/scripts422.sql
@@ -0,0 +1,16 @@
+CREATE TABLE person
+(
+    id      INTEGER,
+    name    TEXT PRIMARY KEY,
+    age     INTEGER CHECK ( age > 0),
+    license BOOLEAN,
+    car_id INTEGER REFERENCES car(id)
+);
+
+CREATE TABLE car
+(
+    id INTEGER PRIMARY KEY,
+    brand TEXT NOT NULL,
+    model TEXT NOT NULL,
+    price NUMERIC
+);
